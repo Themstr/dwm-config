e5cd13ed93e2809313a327d38eb76de7958e3eb9
pulme <maminou.ayadi@icloud.com>
2022-12-22 09:39:21 +0100

    changed config

--- config.def.h
+++ config.def.h
@@ -6,15 +6,14 @@ static const unsigned int gappx     = 8;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "JetBrains Mono Bold:size=10" };
-static const char dmenufont[]       = "JetBrains Mono Bold:size=10";
+static const char *fonts[]          = { "Terminus (TTF):pixelsize=15" };
+static const char dmenufont[]       = "Terminus (TTF):pixelsize=15";
 static const char col_gray1[]       = "#1E1E1E";
 static const char col_gray2[]       = "#383838";
 static const char col_gray3[]       = "#EAEAEA";
 static const char col_gray4[]       = "#1E1E1E";
 static const char col_cyan[]        = "#4EC9B0";
 static const char *colors[][3]      = {
-	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
 	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
 };
@@ -62,8 +61,8 @@ static const char *termcmd[]  = { "kitty", NULL };
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_d,      spawn,          {.v = dmenucmd } },
-	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
+	{ MODKEY,             			XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
@@ -71,7 +70,7 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
-	{ MODKEY,                       XK_Return, zoom,           {0} },
+	{ MODKEY|ShiftMask,             XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ MODKEY,		                XK_w,      killclient,     {0} },
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
