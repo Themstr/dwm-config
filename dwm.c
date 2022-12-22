46e28c4572c210c62c7171be3bb26ce987c27fea
pulme <maminou.ayadi@icloud.com>
2022-12-22 10:16:32 +0100

    removed autostart plugin

--- dwm.c
+++ dwm.c
@@ -195,7 +195,6 @@ static void resizeclient(Client *c, int x, int y, int w, int h);
 static void resizemouse(const Arg *arg);
 static void restack(Monitor *m);
 static void run(void);
-static void runAutostart(void);
 static void scan(void);
 static int sendevent(Client *c, Atom proto);
 static void sendmon(Client *c, Monitor *m);
@@ -1411,11 +1410,6 @@ run(void)
 			handler[ev.type](&ev); /* call handler */
 }
 
- void
-runAutostart(void) {
-	system("cd ~/.dwm; ./autostart.sh &");
-}
-
 void
 scan(void)
 {
@@ -2205,7 +2199,6 @@ main(int argc, char *argv[])
 		die("pledge");
 #endif /* __OpenBSD__ */
 	scan();
-	runAutostart();
 	run();
 	cleanup();
 	XCloseDisplay(dpy);
